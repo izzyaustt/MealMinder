ded5f4c0083257494a4006cff1bf76d471c372ee
Isabella <ibellaaustin@gmail.com>
2025-10-25 19:05:45 -0400

    solving merge conflicts

--- server/index.js
+++ server/index.js
@@ -28,19 +28,10 @@ let fridgeItems = [
   { name: "Bananas", expiration: "2025-10-28" },
 ];
 
-//firebase admin sdk setup
-<<<<<<< HEAD
-import serviceAccount from "./firebase-service-account.json" assert { type: "json" };
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     storageBucket: process.env.FIREBASE_BUCKET
-=======
-import serviceAccount from "./firebase-key.json" assert { type: "json" };
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
->>>>>>> 90d7907e609d7c45cc8145a9582d971783e49bec
 });
 
 //middleware to verify firebase token :p
@@ -66,7 +57,6 @@ app.get("/", (req, res) => {
 
 //ROUTESSS
 // upload route adding ocr
-<<<<<<< HEAD
 app.post("/upload", cors(corsOptions), /*verifyFirebaseToken,*/ upload.single("receipt"), async (req, res) => {
     try{
 
@@ -83,23 +73,6 @@ app.post("/upload", cors(corsOptions), /*verifyFirebaseToken,*/ upload.single("r
 
         fridgeItems.push(...extractedItems);
 
-=======
-app.post("/upload", verifyFirebaseToken, upload.single("receipt"), async (req, res) => {
-    try{
-        const filePath = req.file.path;
-
-        //calling ocr 
-        const extractedLines = await detectText(filePath);
-
-        //convert lines to items
-        const extractedItems = extractedLines.map(line => ({
-            name: line,
-            expiration: "2025-11-03",
-        }));
-
-        fridgeItems.push(...extractedItems);
-
->>>>>>> 90d7907e609d7c45cc8145a9582d971783e49bec
         res.json({ message: "ocr complete", extractedItems, fridgeItems, });
     }catch(err){
     console.error(err);
@@ -107,20 +80,12 @@ app.post("/upload", verifyFirebaseToken, upload.single("receipt"), async (req, r
     }
 });
 
-<<<<<<< HEAD
 app.get("/mock-items", /*verifyFirebaseToken*/ (req,res) => {
-=======
-app.get("/mock-items", verifyFirebaseToken, (req,res) => {
->>>>>>> 90d7907e609d7c45cc8145a9582d971783e49bec
     res.json(fridgeItems);
 });
 
 //adding an item
-<<<<<<< HEAD
 app.post("/add-item", /*verifyFirebaseToken*/ (req,res) => {
-=======
-app.post("/add-item", verifyFirebaseToken, (req,res) => {
->>>>>>> 90d7907e609d7c45cc8145a9582d971783e49bec
     const { name, expiration } =req.body;
     if(!name || !expiration){
         return res.status(400).json({error: "Name and expiration required!"});
@@ -131,14 +96,10 @@ app.post("/add-item", verifyFirebaseToken, (req,res) => {
 });
 
 //deleting an item
-<<<<<<< HEAD
 app.delete("/delete-item/:name", /*verifyFirebaseToken*/ (req, res) => {
-=======
-app.delete("/delete-item/:name", verifyFirebaseToken, (req, res) => {
->>>>>>> 90d7907e609d7c45cc8145a9582d971783e49bec
     const {name} = req.params;
     fridgeItems = fridgeItems.filter( item => item.name !== name);
-    res.json({messgae: "Items deleted!", fridgeItems});
+    res.json({message: "Items deleted!", fridgeItems});
 })
 
 
