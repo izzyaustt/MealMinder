c514690065335a7e70ba6269d625520b2e7aedd9
simone-c18 <151193305+simone-c18@users.noreply.github.com>
2025-10-25 02:41:58 -0400

    Implemented basic login/register pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register'
 import Login from './pages/Login'
-import box from './images/box.png'
-// import Landing from './pages/Landing'
+import Landing from './pages/Landing'
 
 
 function App() {
   return (
     <BrowserRouter>
       <div className="flex flex-col h-screen justify-start items-center">
-          <header className="App-header">
-            <h1 className="text-center text-amber-700">MealMinder</h1>
-            <p>Eliminate food waste :3</p>
-            <img src={box} alt="box" />
-            <Link to="/login">I have an account</Link>
-            <Link to="/register">Register</Link>
-          </header>
-
           <Routes>
-            {/* <Route path="/" element={<App />} /> */}
+            <Route path="/" element={<Landing />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
           </Routes>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,55 @@
 import React from 'react';
+import girl from '../images/girl2.png'
+import '../Login.css';
+import { useForm } from "react-hook-form";
 
-function About() {
+
+function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+      const userData = JSON.parse(localStorage.getItem(data.email));
+      if (userData) {
+          if (userData.password === data.password) {
+              console.log(userData.name + " You Are Successfully Logged In");
+          } else {
+              console.log("Email or Password is not matching with our record");
+          }
+      } else {
+          console.log("Email or Password is not matching with our record");
+      }
+  };
+
   return (
     <div>
-      <h1>About Page</h1>
-      <p>This is the about page of the website.</p>
+      <h1 className="header">Login</h1>
+      <img src={girl} alt="duck mascot" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                    className="input-box"
+                    type="email"
+                    {...register("email", { required: true })}
+                    placeholder="Email"
+                />
+                {errors.email && <span style={{ color: "red" }}>*Email* is mandatory</span>}
+
+                <input
+                    className="input-box"
+                    type="password"
+                    {...register("password", { required: true })}
+                    placeholder="Password"
+                />
+                {errors.password && <span style={{ color: "red" }}>*Password* is mandatory</span>}
+
+                <input type="submit" style={{ backgroundColor: "#AACEA8" }} />
+            </form>
+      
     </div>
   );
 }
 
-export default About;
+export default Login;
