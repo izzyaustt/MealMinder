0adb226f0b2ee43897b01c7473b3acea334e77a7
simone-c18 <151193305+simone-c18@users.noreply.github.com>
2025-10-25 12:51:20 -0400

    Added hamburger nav bar component

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,16 +1,17 @@
 import { Link } from 'react-router-dom';
 import box from '../images/box.png'; 
+import '../App.css'
 
 function Landing() {
   return (
     <header className="App-header">
-      <h1 className="text-center text-amber-700">MealMinder</h1>
+      <h1 className="title">MealMinder</h1>
       <p>Eliminate food waste :3</p>
       <img src={box} alt="box" />
       
       {/* These Link tags allow navigation */}
-      <Link to="/login">I have an account</Link>
-      <Link to="/register">Register</Link>
+      <Link to="/login" className="primary-button">I have an account</Link>
+      <Link to="/register" className="secondary-button">Register</Link>
     </header>
   );
 }
--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import '../styles/upload.css';
 import icon from '../images/upload.png'; 
+import Hamburger from '../components/Hamburger.js';
+
 
 function Upload () {
+  // const [file, setFile] = useState(null);
+  // const [statusMessage, setStatusMessage] = useState('');
 
   const handleFileUpload = (event) => {
     const file = event.target.files[0] 
     console.log(file)
+
+  // // State to manage messages (e.g., success, error)
+  // const [message, setMessage] = React.useState('');
+
+  // const API_ENDPOINT = 'YOUR_BACKEND_API_URL/upload-receipt';
   }
 
 
   return (
     <div className="file-upload">
+      <Hamburger />
         <div className = "upload-container">
           <h1 className="header">Upload Receipt Image</h1>
           <p className="desc">For best results, ensure you are using a plain background and good lighting!</p>
